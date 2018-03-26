fd4423a1a5c08ddbfac5aaa164269baa74550ece
donthaveapps <donthaveapps@gmail.com>
2018-03-26 14:08:18 +0800

    gateway4scanner Read data from usb

--- Gateway4Scanners.py
+++ Gateway4Scanners.py
@@ -24,17 +24,20 @@ Minor Value (length 4 Bytes);
 Measured Power (length 2 Bytes)
 '''
 
+# Parameters
 # Import registered beacon list
 with open("beaconReg.csv", "r") as fBeacon:
     beaconListFull = list(csv.reader(fBeacon))
     beaconNum = [item[0] for item in beaconListFull]
     beaconAddr = [item[1] for item in beaconListFull]
 
+vidpid = ["1A86:7523"]  # TODO: add VID:PID of other devices if required
+
 # TODO: delete after testing
 serialin0 = "00000000:00000000000000000000000000000000:0000000000:FC176623BE92:-071"
 serialin1 = "4C000215:B9407F30F5F8466EAFF925556B57FE6D:AE44AC93B4:F9B5352BDBFB:-084"
 
-
+'''
 # Access USB with pyserial
 # Ref https://stackoverflow.com/questions/8110310/simple-way-to-query-connected-usb-devices-info-in-python
 device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
@@ -49,35 +52,52 @@ for i in df.splitlines():
             devices.append(dinfo)
 print(devices)
 
-# Filter data from registered beacons
+for item in devices:
+    if item.tag == "QinHeng Electronics HL-340 USB-Serial adapter":
+        # TODO: record port number
+'''
+
+# Get relevant device dev path
+devPath = []
+devPath_re = re.compile(r"/dev/tty[\w|\d]*\d")
+for item in vidpid:
+    portListRaw = subprocess.check_output("python3 -m serial.tools.list_ports {}".format(item))  # Obtain dev path for items matching vidpid
+    devPath += devPath_re.findall(portListRaw)
+
+
+# Read and filter data from registered beacons
 # beaconRegex = re.compile(r'([\d|A-G]+):([\d|A-G]+):([\d|A-G]+):([\d|A-G]+):(-\d+)')
 beaconRegex = re.compile(r'([\d|A-G]{8}):([\d|A-G]{32}):([\d|A-G]{4})([\d|A-G]{4})([\d|A-G]{2}):([\d|A-G]{12}):(-\d{3})')
-beaconData = beaconRegex.match(serialin1)
 
-beaconMAC = ""
-for i in range(0, len(beaconData.group(6)), 2):
-    beaconMAC += beaconData.group(6).lower()[i:i+2]+":"
-beaconMAC = beaconMAC[0:-1]
+while True:
+    global devPath
+    for item in devPath:
+        with serial.Serial(item, timeout=2) as ser:  #(item)
+            line = ser.readline()
+            beaconData = beaconRegex.match(line)
 
-beaconDataDict = {}
-if True: #beaconMAC in beaconAddr:
-    beaconDataDict = {"devicegroup": "beaconGateway",
-                      "topic": "scannerStatus",
-                      "project": "J3628",
-                      "scannerId": "DTT-ARD-TST",
-                      "datetime": datetime.datetime.now().isoformat(),
-                      "factoryId": beaconData.group(1),
-                      "ibeaconUuid": beaconData.group(2),
-                      "major": int(beaconData.group(3), 16),
-                      "minor": int(beaconData.group(4), 16),
-                      "measuredPower": int(beaconData.group(5), 16),
-                      "beaconAddr": beaconMAC,
-                      "beaconRssi": beaconData.group(7)
-                      }
+            beaconMAC = ""
+            for i in range(0, len(beaconData.group(6)), 2):
+                beaconMAC += beaconData.group(6).lower()[i:i+2]+":"
+            beaconMAC = beaconMAC[0:-1]
 
-beaconDataJSON = json.dumps(beaconDataDict)
-
-print(beaconDataJSON)
+            beaconDataDict = {}
+            if beaconMAC in beaconAddr:
+                beaconDataDict = {"devicegroup": "beaconGateway",
+                                  "topic": "scannerStatus",
+                                  "project": "J3628",
+                                  "scannerId": "DTT-ARD-TST",
+                                  "datetime": datetime.datetime.now().isoformat(),
+                                  "factoryId": beaconData.group(1),
+                                  "ibeaconUuid": beaconData.group(2),
+                                  "major": int(beaconData.group(3), 16),
+                                  "minor": int(beaconData.group(4), 16),
+                                  "measuredPower": int(beaconData.group(5), 16),
+                                  "beaconAddr": beaconMAC,
+                                  "beaconRssi": beaconData.group(7)
+                                  }
+                beaconDataJSON = json.dumps(beaconDataDict)
+                print(beaconDataJSON)
 # print(beaconAddr)
 
 
